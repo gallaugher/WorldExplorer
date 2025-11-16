f28eef4734cb5fb9e62683b58526c8957993eacc
gallaugher <john.gallaugher@bc.edu>
2025-11-16 17:49:53 -0500

    Initial Commit

---
+++ WorldExplorer/WorldExplorerApp.swift
@@ -0,0 +1,15 @@
+//  WorldExplorerApp.swift
+//  WorldExplorer
+//  Created by John Gallaugher on 11/16/25.
+//  YouTube.com/profgallaugher - gallaugher.bsky.social
+
+import SwiftUI
+
+@main
+struct WorldExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
